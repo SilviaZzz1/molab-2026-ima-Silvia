d5f692fad6a779f79a2889e5fe762d7519f1f118
SilviaZzz1 <silverzhou111@gmail.com>
2026-09-07 13:40:59 -0400

    update1sf

---
+++ Swift- Fundamental- Documentation-SZ/SF001.playground/Pages/variable-constant.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//var = “create a new variable”
+var name = "John"
+name = "Jane"
+name = "Silvia"
+// you can change var as many as you wnat
+
+//let = a constant which you cannot CHANGE
+let character = "S"
+// if you assign
+//character = "A"
+//it will appear error
+ 
+// keep playerName this same
+var playerName = "P1"
+print(playerName)
+
+playerName = "P2"
+print(playerName)
+
+playerName = "P3"
+print(playerName)
+
+//use more let(constant) so you won't done it accidentally
+let manager = "Tom"
+let friend = "Celine"
+
+
+// Variables test
+
+//Which of these lines of code create new variables?
+// A) message = "Good morning, Dave!"
+// B) var operatingSystem = "macOS"
+var operatingSystem = "macOS"
+// B is correct
+// So when starting a new variables needs a var at the beginning.
+
+//Constants Test
+// Which of these lines create constants?
+//A)height = 172.1
+//B)let width = 14.0
+let width = 14.0
+//B
+
+
